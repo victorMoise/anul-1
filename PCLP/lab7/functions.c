aeeff204d5d548ca2606591cb4dd5cbcc9332e15
victorMoise <67602024+victorMoise@users.noreply.github.com>
2022-11-21 10:07:29 +0200

    Update functions.c

--- PCLP/lab7/functions.c
+++ PCLP/lab7/functions.c
@@ -30,10 +30,58 @@ char *create_input_str() {
 }
 
 
-int este_vocala(char c) {
+int is_vowel(char c) {
     char vocale[] = "aeiouAEIOU";
     if (strchr(vocale, c)) {
         return 1;
     }
     return 0;
+}
+
+
+void swap_str(char *str1, char *str2) {
+    char *tmp = (char *)calloc(strlen(str1), sizeof(char));
+    strcpy(tmp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, tmp);
+    free(tmp);
+    return;
+}
+
+
+int is_sufix(char *str1, char *str2) {
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    if (len1 > len2) {
+        return 0;
+    }
+    for (int i = 0; i < len1; i++) {
+        if (str1[len1 - i - 1] != str2[len2 - i - 1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+int remove_leter_at_pos(int i, char *str) {
+    int len = strlen(str);
+    for (int j = i; j < len; j++) {
+        str[j] = str[j + 1];
+    }
+    return len - 1;
+}
+
+
+void swap_int(int *a, int *b) {
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+    return;
+}
+
+
+void replace_with_capital(char *c) {
+    *c = *c - 'a' + 'A';
+    return;
 }
