68d411a8da261dce87357a864b1154d3bb22bba0
victorMoise <67602024+victorMoise@users.noreply.github.com>
2022-11-21 10:06:34 +0200

    Update functions.h

--- PCLP/lab7/functions.h
+++ PCLP/lab7/functions.h
@@ -3,4 +3,9 @@
 size_t my_strlen(char *);
 void my_output_str(char *);
 char *create_input_str();
-int este_vocala(char);
+int is_vowel(char);
+int is_sufix(char *, char *);
+void swap_str(char *, char *);
+int remove_leter_at_pos(int, char *);
+void swap_int(int *, int *);
+void replace_with_capital(char *);
